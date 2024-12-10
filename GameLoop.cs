2dfd9291e0bd18679b496ccac25d23db100cdb96
Elijah Rogers <ejrogersemail@gmail.com>
2024-12-10 16:39:14 -0500

    Player hit or stay method

--- GameLoop.cs
+++ GameLoop.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,7 +15,7 @@ namespace MiniProjectBlackJack
         //Main gameloop
         public static void Run()
         {
-            
+            Deck.CreateDeck();
             Console.WriteLine("WELCOME TO BLACKJACK!");
             while (StartOrEnd())
             {
@@ -22,10 +23,8 @@ namespace MiniProjectBlackJack
                 Player player = new Player(); //New player and Dealer Creation
                 Dealer dealer = new Dealer();
                 Deal(player, dealer);
-                while (PlayerHitOrStay())
-                {
-
-                }
+                PlayerHitOrStay(player);
+                
             }
             
         }
@@ -74,14 +73,14 @@ namespace MiniProjectBlackJack
             card = Deck.ShuffledCardDeck.Pop();
             player.AddToHand(card);
             player.CardValueSum += card.CardValue;
-            Console.WriteLine($"First Card to player: {card.CardName}");
+            Console.WriteLine($"Second Card to player: {card.CardName}");
 
             Console.WriteLine();
             //Last is dealer 2nd card show
             card = Deck.ShuffledCardDeck.Pop();
             dealer.AddToHand(card);
             dealer.CardValueSum += card.CardValue;
-            Console.WriteLine($"First Card to Dealer: {card.CardName}");
+            Console.WriteLine($"Second Card to Dealer: {card.CardName}");
 
             Console.WriteLine("<========== DEAL END ==========>");
 
@@ -93,16 +92,40 @@ namespace MiniProjectBlackJack
             Console.WriteLine();
         }
 
-        //Hit or stay choice of the game
-        static bool PlayerHitOrStay()
+        //Hit or stay choice of the game for player
+        static void PlayerHitOrStay(Player player)
         {
-            Console.WriteLine();
-            Console.WriteLine("Do you wish to HIT or STAY?");
-            return false;
+            bool whileChoice = true;
+            while (whileChoice)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Do you wish to HIT or STAY?");
+                Console.Write("Enter Here:");
+                string choice = Console.ReadLine().Trim().ToLower();
+                if (choice == "hit")
+                {
+                    // Give the player a new card
+                    Card card = Deck.ShuffledCardDeck.Pop();
+                    player.AddToHand(card);
+                    player.CardValueSum += card.CardValue;
+                    Console.WriteLine($"New Card to player: {card.CardName}");
+
+                    Console.WriteLine();
+                    //Print our current player cards and total
+                    Console.Write("Player hand total: ");
+                    player.PrintHand();
+                    Console.Write($"  =  {player.CardValueSum}");
+                    Console.WriteLine();
+
+                }
+                else
+                {
+                    whileChoice = false;
+                }
+            }
+            
         }
 
     }
-
-    
 
 }
--- Program.cs
+++ Program.cs
@@ -4,49 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Deck.CreateDeck();
-
-            //Shuffle and stack check for deck function PASS
-            Deck.PrintDeck();
-            Deck.ShuffleDeck();
-            Console.WriteLine();
-            Deck.PrintDeck();
-            Console.WriteLine();
-            Console.WriteLine(Deck.ShuffledCardDeck.Pop().CardName);
-            Console.WriteLine(Deck.ShuffledCardDeck.Count);
-            Console.WriteLine(Deck.ShuffledCardDeck.Pop().CardName);
-            Console.WriteLine(Deck.ShuffledCardDeck.Count);
-
-            Player player = new Player(); //New player  and Dealer
-            Dealer dealer = new Dealer();
-
-            //Player Check PASS
-            player.AddToHand(Deck.ShuffledCardDeck.Pop());
-            player.AddToHand(Deck.ShuffledCardDeck.Pop());
-            player.AddToHand(Deck.ShuffledCardDeck.Pop());
-            Console.WriteLine();
-            Console.WriteLine();
-            foreach (Card card in player.CurrentCards)
-            {
-                Console.WriteLine($"{card.CardName}");
-            }
-            Console.WriteLine();
-            Console.WriteLine(Deck.ShuffledCardDeck.Count);
-
-            //Dealer check PASS
-            dealer.AddToHand(Deck.ShuffledCardDeck.Pop());
-            dealer.AddToHand(Deck.ShuffledCardDeck.Pop());
-            dealer.AddToHand(Deck.ShuffledCardDeck.Pop());
-            Console.WriteLine();
-            Console.WriteLine();
-            foreach (Card card in dealer.CurrentCards)
-            {
-                Console.WriteLine($"{card.CardName}");
-            }
-            Console.WriteLine();
-            Console.WriteLine(Deck.ShuffledCardDeck.Count);
-
-            GameLoop.Run();
+            GameLoop.Run();       
         }
     }
 }
